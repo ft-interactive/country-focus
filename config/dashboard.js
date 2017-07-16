30dd1c74ca55ecc96da70cb33feb1426648294a9
Christopher Möller <chrtze@gmail.com>
2017-07-16 21:52:30 +0200

    feat(Dashboard): use headline, intro and section from remote json data

--- config/dashboard.js
+++ config/dashboard.js
@@ -2,21 +2,8 @@ import Axios from 'axios';
 
 const config = {
   dataSource: 'https://bertha.ig.ft.com/view/publish/gss/1xFgTur9VO9IUw2yUL_13s1FqFynGzXN_Pc1URYFvE40/countries,indicators',
-  countryName: 'Russia',
 };
 
-export default async () => {
-  Axios({
-    url: config.dataSource,
-    method: 'get',
-    responseType: 'json',
-    transformResponse: (data) => {
-      const result = Object.assign({}, data);
-
-      result.indicators = result.indicators.filter(d => d.country === config.countryName);
-      result.countries = result.countries.find(d => d.country === config.countryName);
-
-      return result;
-    },
-  })
-};
+export default async () => (
+  Axios.get(config.dataSource)
+);
--- config/index.js
+++ config/index.js
@@ -1,13 +1,31 @@
+import axios from 'axios';
+
 import article from './article';
-import getDashConfig from './dashboard';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
+
+const countryName = 'Russia';
 
 export default async () => {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  const dashboard = await getDashConfig();
+  const dashboardData = (await axios('https://bertha.ig.ft.com/view/publish/gss/1xFgTur9VO9IUw2yUL_13s1FqFynGzXN_Pc1URYFvE40/countries,indicators')).data;
+
+  const dashboard = {};
+
+  dashboard.indicators = dashboardData.indicators
+    .filter(indicator => indicator.country === countryName);
+  dashboard.country = dashboardData.countries
+    .find(country => country.country === countryName);
+
+  d.headline = dashboard.country.headline;
+  d.summary = dashboard.country.standfirst;
+
+  d.topic = {
+    name: dashboard.country.sectionname,
+    url: dashboard.country.stream,
+  };
   /*
   An experimental demo that gets content from the API
   and overwrites some model values. This requires the Link File
