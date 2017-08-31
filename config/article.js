e2a67ce180c06ca2dcca41ef78af54ea5b224c94
Martin Stabe <martinstabe@gmail.com>
2017-08-31 11:09:42 +0100

    Update ad zones

--- config/article.js
+++ config/article.js
@@ -99,7 +99,7 @@ export default () => ({ // eslint-disable-line
   //ADVERTISING
   ads: {
     // ad unit hierarchy makes ads more granular. Start with ft.com and /companies /markets /world as appropriate to your story
-    gptAdunit: 'ft.com/world/europe',
+    gptAdunit: 'ft.com//special.reports/focus.romania',
     // granular targeting is optional and will be specified by the ads team
     dftTargeting: '',
   },
