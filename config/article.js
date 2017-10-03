ca61c1a8346927af249ca04a138f463449b9c848
Martin Stabe <martinstabe@gmail.com>
2017-10-03 12:06:28 +0100

    Fixed pub date

--- config/article.js
+++ config/article.js
@@ -14,7 +14,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-09-18T09:40:01.973Z'),
 
   headline: '',
 
